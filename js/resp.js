43b6bbaf19ef7c06ccff3706f2b5cdb8ae1db682
Reshma <reshma.a@Technovert.com>
2022-03-24 15:04:45 +0530

    Responsive

--- js/resp.js
+++ js/resp.js
@@ -1,10 +1,15 @@
 burger = document.querySelector('.navbar-toggler')
 wallfeed = document.querySelector('.wall-feed')
 searchbar=document.querySelector(".search-bar")
+wall=document.querySelector(".wall")
+container=document.querySelector(".container")
 
 burger.addEventListener('click',()=>{
     wallfeed.classList.toggle("v-class-resp");
     searchbar.classList.toggle("v-class-resp");
     wallfeed.classList.toggle("h-nav-resp");
+    wall.classList.toggle("v-class-resp");
+    container.classList.toggle("v-class-resp");
+    wall.classList.toggle("h-nav-resp");
 
 })
